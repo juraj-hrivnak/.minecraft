312466f951e41ed241dc009b7801cfa6bfe953fc
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-04-13 21:04:37 +0200

    Update scripts

--- pax/setup-unix.sh
+++ pax/setup-unix.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# Parse json
 pax_version=$(grep -Po '(?<="PAX": ")[^"]*' setup.json)
 server_pack_creator_version=$(grep -Po '(?<="ServerPackCreator": ")[^"]*' setup.json)
+
 echo "pax_version value is: $pax_version"
 echo "server_pack_creator_version value is: $server_pack_creator_version"
 
-# curl https://github.com/froehlichA/pax/releases/download/v$pax_version/pax-windows.zip -o pax.zip -L -J
+# Download pax
 curl https://github.com/froehlichA/pax/releases/download/v$pax_version/pax -o pax -L -J
 
+# Download server pack creator
 curl https://github.com/Griefed/ServerPackCreator/releases/download/$server_pack_creator_version/ServerPackCreator-$server_pack_creator_version.jar -o ./serverpack/ServerPackCreator-$server_pack_creator_version.jar -L -J
 
-# unzip pax.zip -d '.\'
-# rm pax.zip
-
+# Wait for user response
 read -p "Press any key to continue" x
--- pax/setup-windows.ps1
+++ pax/setup-windows.ps1
@@ -1,10 +1,20 @@
 
+# Parse json
 $json = Get-Content '.\setup.json' | Out-String | ConvertFrom-Json
-$PAXVersion = $json.PAX
-$ServerPackCreatorVersion = $json.ServerPackCreator
+$pax_version = $json.PAX
+$server_pack_creator_version = $json.ServerPackCreator
 
-Invoke-WebRequest https://github.com/froehlichA/pax/releases/download/v$PAXVersion/pax-windows.zip -OutFile pax.zip
+Write-Output "pax_version value is: $pax_version"
+Write-Output "server_pack_creator_version value is: $server_pack_creator_version"
+
+# Download and unzip pax
+Invoke-WebRequest https://github.com/froehlichA/pax/releases/download/v$pax_version/pax-windows.zip -OutFile pax.zip
 Expand-Archive -Path pax.zip -DestinationPath .\
 Remove-Item pax.zip
 
-Invoke-WebRequest https://github.com/Griefed/ServerPackCreator/releases/download/$ServerPackCreatorVersion/ServerPackCreator-$ServerPackCreatorVersion.jar -OutFile ./serverpack/ServerPackCreator-$ServerPackCreatorVersion.jar
+# Download server pack creator
+Invoke-WebRequest https://github.com/Griefed/ServerPackCreator/releases/download/$server_pack_creator_version/ServerPackCreator-$server_pack_creator_version.jar -OutFile ./serverpack/ServerPackCreator-$server_pack_creator_version.jar
+
+# Wait for user response
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
