d19b34f6c08febda5308dcbd5df099e4d253ed1d
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-04-15 03:08:55 +0200

    Update diff util

--- pax/diff.sh
+++ pax/diff.sh
@@ -3,18 +3,24 @@
 depth=1
 
 git checkout main
-mods_removed=$(git diff -U0 {main~${depth},main}:pax/modpack/manifest.json | grep '^-' | grep -P -o '"files":.*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /')
-mods_added=$(git diff -U0 {main~${depth},main}:pax/modpack/manifest.json | grep '^+' | grep -P -o '"files":.*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /')
+mods_removed=$(git diff -U0 {main~${depth},main}:pax/modpack/manifest.json | grep '^-')
+mods_added=$(git diff -U0 {main~${depth},main}:pax/modpack/manifest.json | grep '^+')
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "x---------------x"
-echo "|  Mod Changes  |"
-[ ! -z "$mods_removed" ] && echo -e "${RED}Removed: \n$mods_removed${NC}"
-[ ! -z "$mods_added" ] && echo -e "${GREEN}Added: \n$mods_added${NC}"
-echo "x---------------x"
+echo -e "x---------------x"
+echo -e "|  Mod Changes  |"
+if [ ! -z "$mods_removed" ]; then
+    echo -e "${RED}Removed:"
+    echo -e $mods_removed | grep -P -o '"files":[\s]*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
+fi
+if [ ! -z "$mods_added" ]; then
+    echo -e "${GREEN}Added:"
+    echo -e $mods_added | grep -P -o '"files":[\s]*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
+fi
+echo -e "${NC}x---------------x"
 
 # Wait for user response
 read -p "Done! Press any key to continue" x
