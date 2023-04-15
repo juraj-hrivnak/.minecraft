67a53f4dbb6115c9028465ccaf9cdfa0f619d5ac
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-04-15 04:01:42 +0200

    Update diff generator

--- pax/diff.sh
+++ pax/diff.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 git checkout main
-commit=$(git log --skip 2 --pretty=format:"%h" -n 1 --follow modpack/manifest.json)
+cd ../
+commit=$(git log -n 1 --skip 2 --pretty=format:"%h" -- pax/modpack/manifest.json)
+latest_commit=$(git log -n 1 --skip 2 --pretty=format:"%h" origin/main)
 
-mods_removed=$(git diff -U0 {${commit},main}:pax/modpack/manifest.json | grep '^-')
-mods_added=$(git diff -U0 {${commit},main}:pax/modpack/manifest.json | grep '^+')
+echo $commit
+echo $latest_commit
 
+mods_added=$(git diff -U0 $commit $latest_commit -- pax/modpack/manifest.json | grep '^+')
+mods_removed=$(git diff -U0 $commit $latest_commit -- pax/modpack/manifest.json | grep '^-')
+
+git diff -U0 $commit $latest_commit -- pax/modpack/manifest.json
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# | grep -P -o '"files":[\s]*\[\K[^\]]*'
+# | grep -P -o '"files":[\s]*\[\K[^\]]*'
+
 echo -e "x---------------x"
 echo -e "|  Mod Changes  |"
-if [ ! -z "$mods_removed" ]; then
-    echo -e "${RED}Removed:"
-    echo -e $mods_removed | grep -P -o '"files":[\s]*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
-fi
-if [ ! -z "$mods_added" ]; then
-    echo -e "${GREEN}Added:"
-    echo -e $mods_added | grep -P -o '"files":[\s]*\[\K[^\]]*' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
-fi
+echo -e "${GREEN}Added:"
+echo -e $mods_added | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
+echo -e "${RED}Removed:"
+echo -e $mods_removed | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
 echo -e "${NC}x---------------x"
 
 # Wait for user response
