63a1eb4ea2fcf03220edb1738aae2da15e798cd6
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-04-15 13:17:57 +0200

    Update regex

--- pax/diff.sh
+++ pax/diff.sh
@@ -20,13 +20,17 @@ NC='\033[0m'
 
 # | grep -P -o '"files":[\s]*\[\K[^\]]*.*'
 # | grep -P -o '((?<!"files".)[\s\S])*$'
+# | grep -P -o '((?<!"files"(.|\s)(.|\s))[\s\S])*\]'
+# ((?<!"files"(.|\s)(.|\s))[\s\S])*\]*"name":[\s]*"\K[^"]*.*
+# ((?<="files"(.|\s)(.|\s))[\s\S])*
+# (?<="files"(.|\s)(.|\s).)[\s\S]*\]
 
 echo -e "x---------------x"
 echo -e "|  Mod Changes  |"
 echo -e "${GREEN}Added:"
-echo -e $mods_added | grep -P -o '((?<!"files".)[\s\S])*$' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
+echo -e $mods_added | grep -P -o '(?<="files"(.|\s)(.|\s).)[\s\S]*\]' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
 echo -e "${RED}Removed:"
-echo -e $mods_removed | grep -P -o '((?<!"files".)[\s\S])*$' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
+echo -e $mods_removed | grep -P -o '(?<="files"(.|\s)(.|\s).)[\s\S]*\]' | grep -P -o '"name":[\s]*"\K[^"]*' | sed -e 's/^/- /'
 echo -e "${NC}x---------------x"
 
 # Wait for user response
