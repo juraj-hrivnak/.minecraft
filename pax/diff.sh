93a31321fd3a4afd3950bb5e76bc45e5d264bbeb
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-04-26 20:56:24 +0200

    Print mod changes since latest tag

--- pax/diff.sh
+++ pax/diff.sh
@@ -6,10 +6,14 @@ manifest="pax/modpack/manifest.json"
 branch=$(git rev-parse --abbrev-ref HEAD)
 previous_commit=$(git log -n 1 --skip 1 --pretty=format:"%h" -- $manifest)
 latest_commit=$(git log -n 1 --pretty=format:"%h" $branch -- $manifest)
+latest_tagged_commit=$(git rev-list --tags --max-count=1)
+latest_tag=$(git describe --tags --abbrev=0)
 
 echo "branch: $branch"
 echo "previous commit: $previous_commit"
 echo "latest commit: $latest_commit"
+echo "latest tagged commit: $latest_tagged_commit"
+echo "latest tag: $latest_tag"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
